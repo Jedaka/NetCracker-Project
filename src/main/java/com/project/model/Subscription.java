9aa3152fb8d8c13f1dccb32a0891b219eabe47e9
larinme <maksim_larin@yahoo.com>
2015-11-24 00:36:01 +0300

    check user and episodes to printing

--- src/main/java/com/project/model/Subscription.java
+++ src/main/java/com/project/model/Subscription.java
@@ -64,4 +64,23 @@ public class Subscription {
                 ", studio=" + getStudio() +
                 '}';
     }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Subscription that = (Subscription) o;
+
+        if (id != that.id) return false;
+        return token.equals(that.token);
+
+    }
+
+    @Override
+    public int hashCode() {
+        int result = id;
+        result = 31 * result + token.hashCode();
+        return result;
+    }
 }
